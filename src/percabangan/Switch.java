abb024356c574d2bc7e5ab2c46f4ef051f67b575
khairimadani <khairimadani16@gmail.com>
2020-05-03 22:24:31 +0800

    Percabangan Switch dengan char

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -2,23 +2,23 @@ package percabangan;
 
 public class Switch {
     public static void main(String[] args) {
-        int jalanJalan=0;
-        switch(jalanJalan) {
-            case 1:
-                System.out.println("hyung");
-                break;
-            case 2:
-                System.out.println("senpai");
-                break;
-            case 3:
-                System.out.println("cntip");
-                break;
-            case 4:
-                System.out.println("mkn");
-                break;
+        char nilai = 'D';
+        switch (nilai) {
+            case 'A':
+                System.out.println("Pembeli               : \"samsung dmn mba\"");
+                System.out.println("Mba Sales Oppo cntip  : \"gak beli oppo aja mas?\"");
+            case 'B':
+                System.out.println("Pembeli               : \"oppo A51 nya mba?\"");
+                System.out.println("Mba Sales Oppo cntip  : \"mas jgn ngambil jobdesk saya deh\"");
+            case 'C':
+                System.out.println("Pembeli               : \"mba cntip\"");
+                System.out.println("Mba Sales Oppo cntip  : \"apalagi lu!!\"");
+                System.out.println("Pembeli               : \"knp ga tdur mba cntip\"");
             default:
-                System.out.println("tdr");
-                break;
+                System.out.println("Pembeli               : \"mba ak gamau beli oppo, (｀Д´)\"");
+                System.out.println("Mba Sales Oppo cntip  : \"yasudah\"");
+                System.out.println("Pembeli               : \"ysdh\"");
+                System.out.println("Mba Sales Oppo cntip  : \"(｀Д´)\"");
         }
     }
 }
