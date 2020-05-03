a18334544d0b5df0ac400f915ef68e679316af4b
khairimadani <khairimadani16@gmail.com>
2020-05-03 22:01:04 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak akan dijalankan oleh program//
+
+    String nama = "Khairi Madani";
+    int jumlahSks = 24;
+    double indeksPresentasiKumulatif = 3.45;
+
+    }
