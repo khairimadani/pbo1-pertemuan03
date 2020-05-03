e4be5cc64d0d2c550a1f53b2db3ab443cd2646b3
khairimadani <khairimadani16@gmail.com>
2020-05-03 22:15:28 +0800

    Percabangan Switch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=0;
+        switch(jalanJalan) {
+            case 1:
+                System.out.println("hyung");
+                break;
+            case 2:
+                System.out.println("senpai");
+                break;
+            case 3:
+                System.out.println("cntip");
+                break;
+            case 4:
+                System.out.println("mkn");
+                break;
+            default:
+                System.out.println("tdr");
+                break;
+        }
+    }
+}
